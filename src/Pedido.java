e2b736dc92a071aa433f6685edd6971c8023c8d8
paloma <palomafranzon4c@gmail.com>
2026-04-20 15:00:51 +0200

    cambioo comit

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,36 +1,67 @@
-class Pedido {
-    String cliente;
-    double precioUnitario;
-    int cantidad;
+/**
+ * Clase que representa un pedido de productos.
+ * cambio commit
+ */
 
+class Pedido {
+    /** Nombre del cliente que realiza el pedido. */
+    String cliente;
+    /** Precio unitario del producto. */
+    double precioUnitario;
+    /** Cantidad de productos solicitados. */
+    int cantidad;
+/** Constructor de la clase Pedido. 
+ * @param cliente Nombre del cliente que realiza el pedido.
+ * @param precioUnitario Precio unitario del producto.
+ * @param cantidad Cantidad de productos solicitados. 
+*/
     public Pedido(String cliente, double precioUnitario, int cantidad) {
         this.cliente = cliente;
         this.precioUnitario = precioUnitario;
         this.cantidad = cantidad;
     }
-
+/** 
+ * @return Nombre del cliente que realiza el pedido.
+ */
     public String getCliente() {
         return cliente;
     }
-
+/**
+ * Establece el nombre del cliente que realiza el pedido.   
+ * @param cliente
+ */
     public void setCliente(String cliente) {
         this.cliente = cliente;
     }
-
+/**
+ * Obtiene el precio unitario del producto.
+ * @return
+ */
 
     public double getPrecioUnitario() {
         return precioUnitario;
     }
+    /**
+     * Establece el precio unitario del producto.
+     * @param precioUnitario
+     */
 
     public void setPrecioUnitario(double precioUnitario) {
         this.precioUnitario = precioUnitario;
     }
 
-
+/**
+ * Obtiene la cantidad de productos solicitados.
+ * @return
+ */
     public int getCantidad() {
         return cantidad;
     }
 
+/**
+ * Establece la cantidad de productos solicitados.
+ * @param cantidad
+ */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
